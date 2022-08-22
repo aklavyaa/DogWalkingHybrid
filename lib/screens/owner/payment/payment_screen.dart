f23e1856d61dabaa2f5b9c835c6a11f2423764a3
Prabhjeetkaur515 <84962197+Prabhjeetkaur515@users.noreply.github.com>
2022-08-22 18:23:17 -0400

    Update payment screen

--- lib/screens/owner/payment/payment_screen.dart
+++ lib/screens/owner/payment/payment_screen.dart
@@ -1,13 +1,19 @@
+import 'dart:convert';
+
 import 'package:dog_walker/constants.dart';
 import 'package:dog_walker/models/order_model.dart';
+import 'package:dog_walker/providers/auth_provider.dart';
 import 'package:dog_walker/providers/owner_provider.dart';
 import 'package:dog_walker/screens/owner/features/feedback_screen.dart';
 import 'package:dog_walker/widgets/custom_button.dart';
 import 'package:dog_walker/widgets/custom_textfield.dart';
 import 'package:dog_walker/widgets/success_dialog_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/route_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class PaymentScreen extends StatefulWidget {
   const PaymentScreen({Key? key, required this.order}) : super(key: key);
@@ -97,6 +103,23 @@ class _PaymentScreenState extends State<PaymentScreen> {
           ),
           CustomButton(
             onPressed: () async {
+              // final owner =
+              //     Provider.of<AuthProvider>(context, listen: false).owner!;
+              // final request = BraintreePayPalRequest(amount: '13.37');
+              // final result = await Braintree.requestPaypalNonce(
+              //   tokenizationKey,
+              //   request,
+              // );
+              // if (result != null) {
+              //   showNonce(result, context);
+              // }
+              await completePayment(context);
+              // } catch (e) {
+              //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              //     content: Text(e.toString()),
+              // ));
+              // }
+
               await Provider.of<OwnerProvider>(context, listen: false)
                   .payForWalker(widget.order);
               Get.to(() => SuccessDialogScreen(
@@ -119,3 +142,45 @@ class _PaymentScreenState extends State<PaymentScreen> {
     );
   }
 }
+
+Future<bool> completePayment(BuildContext context) async {
+  var request = BraintreeDropInRequest(
+    tokenizationKey: tokenizationKey,
+    collectDeviceData: true,
+    googlePaymentRequest: BraintreeGooglePaymentRequest(
+      totalPrice: '4.20',
+      currencyCode: 'USD',
+      billingAddressRequired: false,
+    ),
+    paypalRequest: BraintreePayPalRequest(
+      amount: '4.20',
+      displayName: 'Example company',
+    ),
+    cardEnabled: true,
+  );
+  final result = await BraintreeDropIn.start(request);
+  if (result != null) {
+    showNonce(result.paymentMethodNonce, context);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void showNonce(BraintreePaymentMethodNonce nonce, BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (_) => AlertDialog(
+      title: const Text('Payment Verified'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Text('Thank you for your payment!. Wait for the walker to arrive.'),
+        ],
+      ),
+    ),
+  );
+}
+
+String tokenizationKey = 'sandbox_8hxpnkht_kzdtzv2btm4p7s5j';
